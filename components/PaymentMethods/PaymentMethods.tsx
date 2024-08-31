fab3340cf2b780c823091de00f583b708e43efa3
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2024-08-31 21:30:52 +0100

    build payment methods section

---
+++ components/PaymentMethods/PaymentMethods.tsx
@@ -0,0 +1,16 @@
+const PaymentMethods = () => {
+  return (
+    <section className="section main-container ">
+      <div className="section__header">
+        <h2 className="section__title">
+          All major <br /> payment methods
+        </h2>
+        <p className="section__description">
+          We've got all your payments covered
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default PaymentMethods;
