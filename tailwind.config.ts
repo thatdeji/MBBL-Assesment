7d35800c9b4ea3c1dd7d0e42e90a864677c980a4
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2024-08-31 21:10:16 +0100

    chore : custom config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,27 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        button: "linear-gradient(90deg, #3AC52D 0%, #2FBF21 100%)",
+      },
+      boxShadow: {
+        button: "0px 4px 8px rgba(0, 0, 0, 0.15)",
+      },
+      colors: {
+        main: "#6F2BDC",
+        lightPurple: "#AD8AE7",
+        black: "#000229",
+        grey: " #707479",
+        grey2: "#F5F4F8",
+        lightPink: "#FEF3F1",
+        lightBlue: "#DEE8FF",
+        lightIndigo: "#F3E5FD",
+        input: "#D5C5EE",
+      },
+      padding: {
+        footer: "calc(15rem + 2rem)",
+      },
+      margin: {
+        footer: "15rem",
       },
     },
   },
