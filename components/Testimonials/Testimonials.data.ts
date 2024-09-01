8b1f4d9ffb187e7dff41a50f253d46163aa07bba
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2024-09-01 23:17:04 +0100

    feat : implement testimonials

--- components/Testimonials/Testimonials.data.ts
+++ components/Testimonials/Testimonials.data.ts
@@ -5,6 +5,23 @@ export const testimonials = [
       "Is it possible to Love your credit card processor? with TeamForce, yes!",
     name: "Lana Rey",
     position: "Founder & Leader",
+    img: "https://res.cloudinary.com/dexg5uy3d/image/upload/v1725162000/testi-icon-2-2_dfloar.png",
+  },
+  {
+    ratings: 5,
+    content:
+      "Is it possible to Love your credit card processor? with TeamForce, yes!",
+    name: "Micheal H",
+    position: "Founder & Leader",
+    img: "https://res.cloudinary.com/dexg5uy3d/image/upload/v1725162000/testi-icon-2-3_fswyzc.png",
+  },
+  {
+    ratings: 5,
+    content:
+      "Is it possible to Love your credit card processor? with TeamForce, yes!",
+    name: "J. McGhee",
+    position: "Founder & Leader",
+    img: "https://res.cloudinary.com/dexg5uy3d/image/upload/v1725161999/testi-icon-2-1_updic5.png",
   },
   {
     ratings: 5,
@@ -12,33 +29,22 @@ export const testimonials = [
       "Is it possible to Love your credit card processor? with TeamForce, yes!",
     name: "Lana Rey",
     position: "Founder & Leader",
+    img: "https://res.cloudinary.com/dexg5uy3d/image/upload/v1725162000/testi-icon-2-2_dfloar.png",
   },
   {
     ratings: 5,
     content:
       "Is it possible to Love your credit card processor? with TeamForce, yes!",
-    name: "Lana Rey",
+    name: "Micheal H",
     position: "Founder & Leader",
+    img: "https://res.cloudinary.com/dexg5uy3d/image/upload/v1725162000/testi-icon-2-3_fswyzc.png",
   },
   {
     ratings: 5,
     content:
       "Is it possible to Love your credit card processor? with TeamForce, yes!",
-    name: "Lana Rey",
+    name: "J. McGhee",
     position: "Founder & Leader",
-  },
-  {
-    ratings: 5,
-    content:
-      "Is it possible to Love your credit card processor? with TeamForce, yes!",
-    name: "Lana Rey",
-    position: "Founder & Leader",
-  },
-  {
-    ratings: 5,
-    content:
-      "Is it possible to Love your credit card processor? with TeamForce, yes!",
-    name: "Lana Rey",
-    position: "Founder & Leader",
+    img: "https://res.cloudinary.com/dexg5uy3d/image/upload/v1725161999/testi-icon-2-1_updic5.png",
   },
 ];
--- components/Testimonials/Testimonials.tsx
+++ components/Testimonials/Testimonials.tsx
@@ -3,6 +3,7 @@ import { useCallback, useEffect, useState } from "react";
 import { testimonials } from "./Testimonials.data";
 import useEmblaCarousel from "embla-carousel-react";
 import type { EmblaCarouselType } from "embla-carousel";
+import Image from "next/image";
 
 const Testimonials = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
@@ -35,34 +36,67 @@ const Testimonials = () => {
   }, [emblaApi, onInit, onSelect]);
 
   return (
-    <section className="section w-full py-24 bg-main">
-      <div className="main-container">
-        <h2 className="text-center text-5xl text-white font-semibold mb-16">
+    <section className="section w-full py-12 lg:py-24 relative">
+      <div className="h-full w-full absolute top-0 left-0">
+        <Image
+          src="https://res.cloudinary.com/dexg5uy3d/image/upload/v1725161340/testi-bg-2-1_nrdq1l.png"
+          alt=""
+          className="bg-main object-cover"
+          fill
+        />
+      </div>
+      <div className="main-container relative">
+        <h2 className="text-center section__title text-white mb-10 lg:mb-16">
           What people <br /> are saying about us
         </h2>
         <div className="overflow-hidden" ref={emblaRef}>
           <div className="flex">
-            {testimonials.map(({ name, content, ratings, position }, index) => (
-              <div
-                className="pl-6 flex-[1_1_33.3%] flex-shrink-0 min-h-[300px]"
-                key={index}
-              >
-                <article className=" w-full min-h-full flex flex-col gap-3 px-10 py-16 bg-purple-300 bg-opacity-10 backdrop-blur-[10px] rounded-2xl border border-white">
-                  <div className="bg-yellow-400 w-[100px] h-[20px]" />
-                  <p className="text-lg text-white mb-3">{`"${content}"`}</p>
-                  <div className="flex gap-2 items-center">
-                    <div className="bg-white w-12 h-12 rounded-full" />
-                    <div className="flex flex-col gap-1">
-                      <p className="text-lg text-white">{name}</p>
-                      <p className="text-base text-[#BBA3E3]">{position}</p>
+            {testimonials.map(
+              ({ name, content, ratings, position, img }, index) => (
+                <div
+                  className="relative pl-1 md:pl-4 lg:pl-6 flex-[1_0_100%] md:flex-[1_0_50%] lg:flex-[1_0_33.3%] min-h-[320px]"
+                  key={index}
+                >
+                  <article className=" w-full min-h-full flex flex-col gap-3 px-10 py-14 bg-testimonial bg-opacity-10 backdrop-blur-[20px] rounded-[20px] relative">
+                    {/* <div className="h-[102%] w-[102%] absolute top-0 left-0 z-[-1]">
+                    <Image
+                      src="https://res.cloudinary.com/dexg5uy3d/image/upload/v1725161457/BODY_ssz4ca.png"
+                      alt=""
+                      className="bg-main object-cover"
+                      fill
+                    />
+                  </div> */}
+                    <div className="flex gap-1">
+                      {Array(5)
+                        .fill(null)
+                        .map((_, index) => (
+                          <div key={index} className="h-4 w-4 relative">
+                            <Image
+                              src="https://res.cloudinary.com/dexg5uy3d/image/upload/v1725162440/download_ampo8x.png"
+                              alt=""
+                              className="object-cover"
+                              fill
+                            />
+                          </div>
+                        ))}
                     </div>
-                  </div>
-                </article>
-              </div>
-            ))}
+                    <p className="text-lg text-white mb-5">{`"${content}"`}</p>
+                    <div className="flex gap-2 items-center">
+                      <div className="w-12 h-12 rounded-full overflow-hidden relative">
+                        <Image fill alt="" src={img} />
+                      </div>
+                      <div className="flex flex-col">
+                        <p className="text-lg text-white">{name}</p>
+                        <p className="text-sm text-[#BBA3E3]">{position}</p>
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              )
+            )}
           </div>
         </div>
-        <div className="flex justify-center items-center gap-3 mt-12">
+        <div className="flex justify-center items-center gap-2 lg:gap-3 mt-4 lg:mt-12">
           {scrollSnaps.map((_, index) => (
             <button
               key={index}
