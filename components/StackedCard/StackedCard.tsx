b8103ad2fa303d799ba45253aea87f0ab89e88af
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2024-09-01 23:16:47 +0100

    feat : build stacked cards

--- components/StackedCard/StackedCard.tsx
+++ components/StackedCard/StackedCard.tsx
@@ -9,16 +9,27 @@ const StackedCard: React.FC<IStackedCardProps> = ({ icon, title, variant }) => {
       : variant === "three"
       ? "bg-[#F89FBF]"
       : "bg-[#B085F8]";
+
+  const bgGradient =
+    variant === "one"
+      ? "bg-stackedOne"
+      : variant === "two"
+      ? "bg-stackedTwo"
+      : variant === "three"
+      ? "bg-stackedThree"
+      : "bg-stackedFour";
   return (
-    <article className="relative flex-1 min-h-[300px]">
+    <article className="relative w-full h-[200px] lg:h-[280px]">
       <div
-        className={`absolute inset-0 transform -rotate-12 ${bgColor} rounded-2xl w-full min-h-full`}
+        className={`absolute inset-0 transform -rotate-12 ${bgGradient} rounded-2xl w-full h-full shadow-stackedCard`}
       ></div>
       <div
-        className={`relative backdrop-blur-[10px] ${bgColor} bg-opacity-10 rounded-2xl shadow-custom px-8 py-10 flex flex-col items-start justify-between w-full min-h-full`}
+        className={`relative backdrop-blur-[20px] ${bgColor} bg-opacity-10 rounded-2xl shadow-custom px-8 py-10 flex flex-col items-start justify-between w-full h-full min-h-full`}
       >
-        <div className="text-4xl font-semibold mb-4">{icon}</div>
-        <p className="text-xl font-semibold text-gray-800">{title}</p>
+        <div className="mb-4">{icon}</div>
+        <p className="text-xl lg:text-2xl font-semibold text-gray-800">
+          {title}
+        </p>
       </div>
     </article>
   );
