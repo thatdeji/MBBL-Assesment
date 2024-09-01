20ff8704e70733bb015ab00d08fd1f5941b7516d
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2024-09-01 23:11:31 +0100

    feat : update contact form

--- components/ContactForm/ContactForm.tsx
+++ components/ContactForm/ContactForm.tsx
@@ -1,19 +1,30 @@
 import Button from "$/components/Button/Button";
+import Image from "next/image";
 
 const ContactForm = () => {
   return (
-    <form className="bg-main main-container py-10 rounded-[40px] -mb-footer">
-      <div className="flex flex-col gap-3 max-w-[600px] mx-auto items-center">
-        <input className="input" placeholder="Full name" />
-        <input className="input" placeholder="Email address" />
-        <input className="input" placeholder="Phone number" />
-        <textarea
-          className="textarea"
-          placeholder="Enter your message"
-        ></textarea>
-        <Button className="w-fit">Get Started Now</Button>
-      </div>
-    </form>
+    <section className="main-container">
+      <form className="relative py-10 px-4 rounded-[40px] -mb-footer overflow-hidden">
+        <div className="h-full w-full absolute top-0 left-0">
+          <Image
+            src="https://res.cloudinary.com/dexg5uy3d/image/upload/v1725161340/testi-bg-2-1_nrdq1l.png"
+            alt=""
+            className="bg-main object-cover"
+            fill
+          />
+        </div>
+        <div className="relative flex flex-col gap-3 max-w-[600px] mx-auto items-center">
+          <input className="input" placeholder="Full name" />
+          <input className="input" placeholder="Email address" />
+          <input className="input" placeholder="Phone number" />
+          <textarea
+            className="textarea"
+            placeholder="Enter your message"
+          ></textarea>
+          <Button className="w-fit">Get Started Now</Button>
+        </div>
+      </form>
+    </section>
   );
 };
 
