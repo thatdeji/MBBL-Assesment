b50899a718cd4af646e6ae57b490099a8b27efaa
Akindeji Ayedun <akindejioluwaseun015@gmail.com>
2024-09-03 15:02:14 +0100

    hover state

--- components/StackedCard/StackedCard.tsx
+++ components/StackedCard/StackedCard.tsx
@@ -19,9 +19,9 @@ const StackedCard: React.FC<IStackedCardProps> = ({ icon, title, variant }) => {
       ? "bg-stackedThree"
       : "bg-stackedFour";
   return (
-    <article className="relative w-full h-[200px] lg:h-[280px]">
+    <article className="relative w-full h-[200px] lg:h-[280px] group">
       <div
-        className={`absolute inset-0 transform -rotate-12 ${bgGradient} rounded-2xl w-full h-full shadow-stackedCard`}
+        className={`absolute inset-0 transform -rotate-12 group-hover:rotate-0 transition-transform duration-500 ${bgGradient} rounded-2xl w-full h-full shadow-stackedCard`}
       ></div>
       <div
         className={`relative backdrop-blur-[20px] ${bgColor} bg-opacity-10 rounded-2xl shadow-custom px-8 py-10 flex flex-col items-start justify-between w-full h-full min-h-full`}
